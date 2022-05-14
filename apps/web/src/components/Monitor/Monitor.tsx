8817cef207c560eedb36b7536fd87a2445f30b42
Dean Sharon <deanshrn@gmail.com>
2022-05-15 01:35:49 +0300

    Web: update monitor page layout (#3)
    Co-authored-by: Dean Sharon <deanshrn@gmain.com>

--- apps/web/src/components/Monitor/Monitor.tsx
+++ apps/web/src/components/Monitor/Monitor.tsx
@@ -1,6 +1,6 @@
-import React, { useEffect, useState } from 'react';
-import { MonitorPayload } from '../../interfaces/app.interfaces';
-
+import React, {useEffect, useState} from 'react';
+import {MonitorPayload} from '../../interfaces/app.interfaces';
+import Layout from "../Layout/Layout";
 
 const Monitor: React.FC = () => {
     const [monitorPayload, setMonitorPayload] = useState<MonitorPayload>();
@@ -15,38 +15,53 @@ const Monitor: React.FC = () => {
         setMonitorPayload(payload)
     }
 
-
-
-
     return (
-        <div>
-            <div>
-                Event on contract: {monitorPayload?.events.map((e) => <span >{e}</span >)}
+        <Layout>
+            <div className="flex flex-col">
+                <div className="overflow-x-auto sm:-mx-6 lg:-mx-8">
+                    <div className="py-2 inline-block min-w-full sm:px-6 lg:px-8">
+                        <div className="overflow-hidden">
+                            <table className="min-w-full">
+                                <thead className="bg-black">
+                                    <tr>
+                                        <th scope="col" className="text-md font-medium text-white px-6 py-4 text-left">
+                                            #
+                                        </th>
+                                        <th scope="col" className="text-md font-medium text-white px-6 py-4 text-left">
+                                            Account
+                                        </th>
+                                        <th scope="col" className="text-md font-medium text-white px-6 py-4 text-left">
+                                            HBAR (ℏ)
+                                        </th>
+                                        <th scope="col" className="text-md font-medium text-white px-6 py-4 text-left">
+                                            Events
+                                        </th>
+                                    </tr>
+                                </thead>
+
+                                <tbody>
+                                    <tr className="bg-gray-900 border-b border-gray-800">
+                                        <td className="px-6 py-4 whitespace-nowrap text-md font-medium text-white">1</td>
+                                        <td className="text-md text-white font-light px-6 py-4 whitespace-nowrap">Contract</td>
+                                        <td className="text-md text-white font-light px-6 py-4 whitespace-nowrap">{monitorPayload?.contractBalnce}</td>
+                                        <td className="text-md text-white font-light px-6 py-4 whitespace-nowrap"></td>
+                                    </tr>
+
+                                    <tr className="bg-gray-900 border-b border-gray-800">
+                                        <td className="px-6 py-4 whitespace-nowrap text-md font-medium text-white">2</td>
+                                        <td className="text-md text-white font-light px-6 py-4 whitespace-nowrap">{monitorPayload?.accounts[monitorPayload?.accounts.length - 1].name} Subscriber</td>
+                                        <td className="text-md text-white font-light px-6 py-4 whitespace-nowrap">{monitorPayload?.accounts[monitorPayload?.accounts.length - 1].amount}</td>
+                                        <td className="text-md text-white font-light px-6 py-4 whitespace-nowrap">{monitorPayload?.events.map((eventType) => eventType)}</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
             </div>
 
 
-            <table className="table-auto">
-                <thead>
-                    <tr>
-                        <th>Account</th>
-                        <th>Hbar's</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>Contract</td>
-                        <td>{monitorPayload?.contractBalnce}</td>
-                    </tr>
-
-                    <tr>
-                        <td>{monitorPayload?.accounts[monitorPayload?.accounts.length - 1].name} Subscriber</td>
-                        <td>{monitorPayload?.accounts[monitorPayload?.accounts.length - 1].amount}</td>
-                    </tr>
-                </tbody>
-            </table>
-
-
-        </div >
+        </Layout>
     );
 }
 
