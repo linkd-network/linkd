43bdf6be72a49c5ca955ce079e071b201fa18ecb
MatanShushan <matan22993@gmail.com>
2022-05-15 01:37:57 +0300

    Web: Remove mock data

--- apps/web/src/components/SubscribePage/SubscribePage.tsx
+++ apps/web/src/components/SubscribePage/SubscribePage.tsx
@@ -4,52 +4,7 @@ import AdItem from '../AdItem/AdItem';
 import Layout from "../Layout/Layout";
 
 const SubscribePage: React.FC = () => {
-    const [ads, setAds] = useState<Ad[]>([
-        {
-            id: "210fbe09-df4e-42cb-a7cf-dac32bceb25e",
-            resourceType: "img",
-            title: "Linkd",
-            contentURL: "https://linked.netlify.app/pattern-dark.svg",
-            budget: 1000,
-            costPerAction: 5,
-            triggerType: "Click",
-            destinationURL: "https://linked.netlify.app/",
-            creationDate: "2022-05-14T20:38:41.706Z",
-        },
-        {
-            id: "210fbe09-df4e-42cb-a7cf-dac32bceb25e",
-            resourceType: "img",
-            title: "Linkd",
-            contentURL: "https://linked.netlify.app/pattern-dark.svg",
-            budget: 1000,
-            costPerAction: 5,
-            triggerType: "Click",
-            destinationURL: "https://linked.netlify.app/",
-            creationDate: "2022-05-14T20:38:41.706Z",
-        },
-        {
-            id: "210fbe09-df4e-42cb-a7cf-dac32bceb25e",
-            resourceType: "img",
-            title: "Linkd",
-            contentURL: "https://linked.netlify.app/pattern-dark.svg",
-            budget: 1000,
-            costPerAction: 5,
-            triggerType: "Click",
-            destinationURL: "https://linked.netlify.app/",
-            creationDate: "2022-05-14T20:38:41.706Z",
-        },
-        {
-            id: "210fbe09-df4e-42cb-a7cf-dac32bceb25e",
-            resourceType: "img",
-            title: "Linkd",
-            contentURL: "https://linked.netlify.app/pattern-dark.svg",
-            budget: 1000,
-            costPerAction: 5,
-            triggerType: "Click",
-            destinationURL: "https://linked.netlify.app/",
-            creationDate: "2022-05-14T20:38:41.706Z",
-        }
-    ]);
+    const [ads, setAds] = useState<Ad[]>([]);
 
     useEffect(() => {
         getAds();
